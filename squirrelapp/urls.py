727afbe533c51294d1b13a43345f9e936dd74f5c
Nishita Lamba <Nishita13011998@gmail.com>
2019-12-07 22:42:14 +0000

    changes to urls

--- squirrelapp/urls.py
+++ squirrelapp/urls.py
@@ -1,7 +1,11 @@
+from django.contrib import admin
 from django.urls import path
 
-from . import views
+from tracker import views
 
 urlpatterns = [ 
-    path('',views.index)
+    path('',views.view_all),
+    path('stats/',views.stats),
+    path('map/',views.coordinates),
+    path('<sqatr:Unique_Squirrel_Id>/edit/',views.edit_squirrel),
 ]
