72f79178000007576275080d607c1ca737fad5f6
NishitaLamba <56944992+NishitaLamba@users.noreply.github.com>
2019-12-09 01:34:18 -0500

    Update urls.py

--- squirreltracker/urls.py
+++ squirreltracker/urls.py
@@ -19,6 +19,6 @@ from django.urls import include,path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include ('squirrel_tracker.urls')),
+    path('',include ('squirrelapp.urls')),
     
 ]
