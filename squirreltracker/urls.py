3171b490ed66d8578705c63481b9e2ec103c4a2b
NishitaLamba <56944992+NishitaLamba@users.noreply.github.com>
2019-12-09 01:33:38 -0500

    Update urls.py

--- squirreltracker/urls.py
+++ squirreltracker/urls.py
@@ -13,16 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import include,path
 
 urlpatterns = [
-    path('squirrelapp/', include('Squirrelapp.urls')),   
     path('admin/', admin.site.urls),
-    path('views/',views.index),
-    path('map/',views.coordinates),
-    path('update/',views.edit_squirrel),
-    path('add/', views.add_squirrel),
-    path('stats/',views.stats),  
+    path('',include ('squirrel_tracker.urls')),
+    
 ]
-
