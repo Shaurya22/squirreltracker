28749a899d20dbc1289d7ec737c514db00d0634a
NishitaLamba <56944992+NishitaLamba@users.noreply.github.com>
2019-12-09 00:46:42 -0500

    Update urls.py

--- squirreltracker/urls.py
+++ squirreltracker/urls.py
@@ -17,11 +17,10 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('squirrelapp/', include('squirrelapp.urls')),
-    path('admin/', admin.site.urls),
     path('views/',views.index),
     path('map/',views.coordinates),
+    path('update/',views.edit_squirrel),
     path('add/', views.add_squirrel),
     path('stats/',views.stats),
-    path('update/',views.edit_squirrel)
+    
 ]
