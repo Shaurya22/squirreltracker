5921d32efc10c8e69f1e5c6a572645dfe7f250c7
NishitaLamba <56944992+NishitaLamba@users.noreply.github.com>
2019-12-09 01:31:37 -0500

    Update urls.py

--- squirrelapp/urls.py
+++ squirrelapp/urls.py
@@ -1,13 +1,12 @@
-from django.contrib import admin
 from django.urls import path
-
+  
 from . import views
-
-urlpatterns = [ 
-        path('',views.view_all),
-        path('stats/',views.stats),
-        path('map/',views.coordinates),
-        path('<str:Unique_Squirrel_Id>/edit/',views.edit_squirrel),
-]
-
-
+app_name = 'squirrelapp'
+urlpatterns = [
+    path('',views.main_page, name= 'main'),
+    path('map', views.get_map, name='map'),
+    path('sightings/',views.get_sighting, name='sighting'),
+    path('sightings/<str:Unique_squirrel_ID>/',views.edit_squirrel, name = 'edit_sighting'),
+    path('sightings/add',views.add_squirrel, name = 'add_sighting'),
+    path('sightings/stats', views.get_stats, name = 'stats'),
+    ]
