0540596d5e9a4a93cd0f27b13442cf95c7143b78
Nishita Lamba <Nishita13011998@gmail.com>
2019-12-09 07:18:19 +0000

    modified

--- squirrelapp/urls.py
+++ squirrelapp/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
         path('',views.view_all),
-        path('stats/',views.stats),
         path('map/',views.coordinates),
         path('<str:Unique_Squirrel_Id>/edit/',views.edit_squirrel),
+        path('stats/',views.stats),
 ]
