0469727398d8d6f0f2fc4763ff23b24f14fb21f8
Shaurya Jadeja <shauryajadeja@gmail.com>
2019-12-09 18:06:26 +0000

    debug add

--- squirrelapp/urls.py
+++ squirrelapp/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
         path('',views.view_all),
         path('map/',views.coordinates),
+        path('add/', views.add_squirrel),
         path('<str:Unique_Squirrel_Id>/edit/',views.edit_squirrel),
         path('stats/',views.stats),
 ]
