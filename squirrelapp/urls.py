e18883de5b2989785b05a341812aa63d437b1e23
Shaurya Jadeja <shauryajadeja@gmail.com>
2019-12-10 08:09:47 +0000

    checked error in edit

--- squirrelapp/urls.py
+++ squirrelapp/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
         path('views/',views.index),
         path('map/',views.coordinates),
         path('add/', views.add_squirrel),
-        path('<str:Unique_Squirrel_ID>/edit/',views.edit_squirrel),
+        path('views/<str:Unique_Squirrel_ID>/edit/',views.edit_squirrel),
         path('stats/',views.stats),
 ]
